018da9e8cd823ac6ceb116acaec3b310cf233cd1
StanislavIvanov7 <stanislav772000@abv.bg>
2024-06-05 17:56:57 +0300

    Implemented all method for auction comment controller in administrator area

---
+++ AuctionSystem/Areas/Administrator/Controllers/AuctionCommentController.cs
@@ -0,0 +1,22 @@
+﻿using AuctionSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionSystem.Areas.Administrator.Controllers
+{
+    public class AuctionCommentController : AdministartorBaseController
+    {
+        private readonly IAuctionCommentService auctionCommentService;
+        public AuctionCommentController(IAuctionCommentService _auctionCommentService)
+        {
+            auctionCommentService = _auctionCommentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var model = await auctionCommentService.AllCommentsAsync();
+
+            return View(model);
+        }
+    }
+}
