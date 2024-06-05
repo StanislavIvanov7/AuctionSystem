5a7a2258e4acc5c654876918172e0ffe08cb1d85
StanislavIvanov7 <stanislav772000@abv.bg>
2024-06-05 18:08:59 +0300

    Implemented all method for auction controller in administrator area

---
+++ AuctionSystem/Areas/Administrator/Controllers/AuctionController.cs
@@ -0,0 +1,36 @@
+﻿using AuctionSystem.Core.Contracts;
+using AuctionSystem.Core.Models.Auction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionSystem.Areas.Administrator.Controllers
+{
+    public class AuctionController : AdministartorBaseController
+    {
+        private readonly IAuctionService auctionService;
+
+
+
+        public AuctionController(IAuctionService service)
+        {
+            auctionService = service;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All([FromQuery] AuctionQueryViewModel model)
+        {
+            var auctions = await auctionService.AllAuctionAsync(
+                model.Condition,
+                model.SearchTerm,
+                model.Sorting,
+                model.CurrentPage,
+                model.AuctionPerPage);
+
+            model.TotalAuctionCount = auctions.TotalAuctionCount;
+            model.Auction = auctions.Auction;
+            model.Conditions = await auctionService.AllConditionNamesAsync();
+
+            return View(model);
+        }
+    }
+}
