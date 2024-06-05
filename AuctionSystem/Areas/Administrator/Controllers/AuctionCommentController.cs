b6b51dbf8e53bd5110126bd7b4e290ed8e8098b1
StanislavIvanov7 <stanislav772000@abv.bg>
2024-06-05 17:58:46 +0300

    Implemented delete methods for auction comment controller in administrator area

--- AuctionSystem/Areas/Administrator/Controllers/AuctionCommentController.cs
+++ AuctionSystem/Areas/Administrator/Controllers/AuctionCommentController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionSystem.Core.Contracts;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AuctionSystem.Areas.Administrator.Controllers
 {
@@ -17,6 +18,44 @@ namespace AuctionSystem.Areas.Administrator.Controllers
             var model = await auctionCommentService.AllCommentsAsync();
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await auctionCommentService.ExistAsync(id) == false)
+            {
+                return BadRequest();
+            }
+
+            if (User.IsModerator() == false && User.IsAdmin() == false)
+            {
+                return Unauthorized();
+            }
+
+            var model = await auctionCommentService.GetCommentForDeleteAsync(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            if (await auctionCommentService.ExistAsync(id) == false)
+            {
+                return BadRequest();
+            }
+
+            if (User.IsModerator() == false && User.IsAdmin() == false)
+            {
+                return Unauthorized();
+            }
+
+            await auctionCommentService.RemoveAsync(id);
+
+            return RedirectToAction(nameof(All));
+
         }
     }
 }
